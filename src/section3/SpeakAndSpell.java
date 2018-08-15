c26b00208c3176750b3c37151132c87caf7223d1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-15 13:56:56 -0700

    Day 3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,16 +9,45 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+		speak("pneumonoultramicroscopicsilicovolcanoconiosis");
 		// 2. Catch the user's answer in a String
-
+String answer= JOptionPane.showInputDialog("spell the word");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(answer.equalsIgnoreCase("pneumonoultramicroscopicsilicovolcanoconiosis")) {
+	speak("correct");
+}
 		// 4. Otherwise say "wrong"
+else {
+	speak("wrong");
+}
 
 		// 5. repeat the process for other words
+speak("respiratory");
+// 2. Catch the user's answer in a String
+String response= JOptionPane.showInputDialog("spell the word");
+// 3. If the user spelled the word correctly, speak "correct"
+if(response.equalsIgnoreCase("respiratory")) {
+speak("correct");
+}
+// 4. Otherwise say "wrong"
+else {
+speak("wrong");
+}
+speak("hi");
+// 2. Catch the user's answer in a String
+String memes= JOptionPane.showInputDialog("spell the word");
+// 3. If the user spelled the word correctly, speak "correct"
+if(memes.equalsIgnoreCase("hi")) {
+speak("correct");
+}
+// 4. Otherwise say "wrong"
+else {
+speak("wrong");
+}
+
 		
 	}
+	
 
 	static void speak(String words) {
 		try {
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,37 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
+		int score=0;
 		
 		// 2.  Ask the user a question 
+		String question= JOptionPane.showInputDialog("What planet other than saturn has rings?");
 		
 		// 3.  Use an if statement to check if their answer is correct
+		if(question.equalsIgnoreCase("Uranus")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		}
+			else {
+				JOptionPane.showMessageDialog(null, "incorrect");
+			}
+		
 		
 		// 4.  if the user's answer is correct
 		
 		// -- add one to their score 
-		
+		score ++;
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		if
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
+
 }
+
+
